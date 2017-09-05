ed9e055efab7c647d05447f680b19477608146f6
idmakers <idmakers@gmail.com>
2017-09-05 16:55:34 +0800

    FUCKING OCR DONE 幹幹幹 開始分析藥單 輸入資料庫

--- app/src/main/java/com/example/lab714_pc/drug/OCR.java
+++ app/src/main/java/com/example/lab714_pc/drug/OCR.java
@@ -9,6 +9,7 @@ import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,14 +26,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class OCR extends AppCompatActivity {
+import static com.example.lab714_pc.drug.R.id.OCRTextView;
 
-    Bitmap image;
+public class OCR extends AppCompatActivity implements View.OnClickListener {
+
+    static Bitmap image;
     private TessBaseAPI mTess;
     String datapath = "";
-    private static final int GUI_OK = 0x101;
-    private static String ResultMsg ="";
-
+    private  Button button,btOCR;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,42 +42,41 @@ public class OCR extends AppCompatActivity {
         //init image
 
 
-            //initialize Tesseract API
-            String language = "eng";
-            datapath = getFilesDir()+ "/tesseract/";
-            mTess = new TessBaseAPI();
+        //initialize Tesseract API
+        String language = "eng";
+        datapath = getFilesDir() + "/tesseract/";
+        mTess = new TessBaseAPI();
 
-            checkFile(new File(datapath + "tessdata/"));
+        checkFile(new File(datapath + "tessdata/"));
 
-            mTess.init(datapath, language);
-            Button button = (Button)findViewById(R.id.b01);
-            button.setText("選擇圖片");
-            Button btOCR = (Button)findViewById(R.id.OCRbutton);
-            btOCR.setText("RUN OCR");
-            btOCR.setOnClickListener(new Button.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                processImage();
-            }
-        });
-            button.setOnClickListener(new Button.OnClickListener()
-            {
-                        @Override
-                        public void onClick(View v) {
-                            Intent intent = new Intent();
-                            /* 開啟Pictures畫面Type設定為image */
-                            intent.setType("image/*");
-                            /* 使用Intent.ACTION_GET_CONTENT這個Action */
-                            intent.setAction(Intent.ACTION_GET_CONTENT);
-                            /* 取得相片後返回本畫面 */
-                            startActivityForResult(intent, 1);
-                        }
-
-
-            });
+        mTess.init(datapath, language);
+        button = (Button) findViewById(R.id.b01);
+        button.setOnClickListener(this);
+        button.setText("選擇圖片");
+        btOCR = (Button) findViewById(R.id.OCRbutton);
+        btOCR.setText("RUN OCR");
+        btOCR.setOnClickListener(this);
 
     }
 
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.b01:
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1);
+                break;
+            case R.id.OCRbutton:
+               new ProcessImage().execute("");
+
+
+
+        }
+
+    }
         @Override
         protected void onActivityResult(int requestCode, int resultCode, Intent data) {
             if (resultCode == RESULT_OK) {
@@ -95,15 +95,7 @@ public class OCR extends AppCompatActivity {
             super.onActivityResult(requestCode, resultCode, data);
         }
 
-    public void processImage(){
-        String OCRresult = null;
-        mTess.setImage(this.image);
-        OCRresult = mTess.getUTF8Text();
-        TextView OCRTextView = (TextView) findViewById(R.id.OCRTextView);
-        OCRTextView.setText(OCRresult);
-        mTess.clear();
-        mTess.end();
-    }
+
 
     private void checkFile(File dir) {
         if (!dir.exists()&& dir.mkdirs()){
@@ -147,5 +139,50 @@ public class OCR extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    public class ProcessImage extends AsyncTask<String,String, String> {
+        String OCRresult = null;
+
+        @Override
+        protected String doInBackground(String... args) {
+
+
+                try {
+
+                    mTess.setImage(OCR.image);
+                    OCRresult = mTess.getUTF8Text();
+                    mTess.end();
+                } catch (RuntimeException e) {
+                    Log.e("OcrRecognizeAsyncTask",
+                            "Caught RuntimeException in request to Tesseract. Setting state to CONTINUOUS_STOPPED.",
+                            e);
+
+                    try {
+                        mTess.clear();
+                    } catch (NullPointerException e1) {
+                        // Continue
+                    }
+                    return null;
+                }
+
+            return "Executed";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView txt = (TextView) findViewById(OCRTextView);
+            txt.setText("Executed"); // txt.setText(result);
+            if(OCRresult!=null) {
+                txt.setText(OCRresult);
+            }
+            // might want to change "executed" for the returned string passed
+            // into onPostExecute() but that is upto you
+        }
+
+        @Override
+        protected void onPreExecute() {}
+
+
+
     }
 }
